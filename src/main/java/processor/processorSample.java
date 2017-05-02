4f8d4f3d73a653783fe3917767fc732df0640dcd
Morioka Naoki <n.morioka0@gmail.com>
2017-05-02 23:59:34 +0900

    Add processor sample

--- src/main/java/processor/processorSample.java
+++ src/main/java/processor/processorSample.java
@@ -1,7 +1,21 @@
 package processor;
 
+import io.reactivex.processors.PublishProcessor;
+
 /**
  * Created by moriokanaoki on 2017/04/28.
  */
 public class processorSample {
+    public static void main(String[] args) {
+        PublishProcessor<String> processor = PublishProcessor.create();
+        processor.subscribe(s -> {
+            int i = Integer.parseInt(s);
+            System.out.println("number : " + i);
+        });
+
+        processor.onNext("1");
+        processor.onNext("2");
+        processor.onNext("3");
+        processor.onNext("4");
+    }
 }
