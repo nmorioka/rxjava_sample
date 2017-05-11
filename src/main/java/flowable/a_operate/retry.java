03442c1c3acccd6ac80aeaa48dcd9fe7208221bc
Morioka Naoki <n.morioka0@gmail.com>
2017-05-11 17:02:59 +0900

    retryの追加

---
+++ src/main/java/flowable/a_operate/retry.java
@@ -0,0 +1,25 @@
+package flowable.a_operate;
+
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+
+public class retry {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable.range(1, 10)
+                .observeOn(Schedulers.computation())
+                .flatMap(v -> {
+                    return Flowable.just(v)
+                            .map(w -> w * w);
+                })
+                .retry(3, throwable -> {
+                    // retry可否コード
+                    return true;
+                })
+                .subscribe(System.out::println);
+        Thread.sleep(1000L);
+    }
+    
+}
