4ae5d9e406a31248f2090a0e26ba3d19f8d27469
Morioka Naoki <n.morioka0@gmail.com>
2017-05-23 17:23:24 +0900

    modify processorSample

--- src/main/java/processor/processorSample.java
+++ src/main/java/processor/processorSample.java
@@ -7,15 +7,18 @@ import io.reactivex.processors.PublishProcessor;
  */
 public class processorSample {
     public static void main(String[] args) {
-        PublishProcessor<String> processor = PublishProcessor.create();
-        processor.subscribe(s -> {
-            int i = Integer.parseInt(s);
-            System.out.println("number : " + i);
+        PublishProcessor<Integer> processor = PublishProcessor.create();
+        processor.subscribe(num -> {
+                    System.out.println("number1 : " + num);
+                });
+        processor.map(v -> v * v)
+                .subscribe(num -> {
+            System.out.println("number2 : " + num);
         });
 
-        processor.onNext("1");
-        processor.onNext("2");
-        processor.onNext("3");
-        processor.onNext("4");
+        processor.onNext(1);
+        processor.onNext(2);
+        processor.onNext(3);
+        processor.onNext(4);
     }
 }
