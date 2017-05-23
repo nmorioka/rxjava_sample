d64aa044d55689d38dc730f46e6ee7b57e5f100c
Morioka Naoki <n.morioka0@gmail.com>
2017-05-23 17:24:49 +0900

    modify flatMap

--- src/main/java/flowable/a_operate/flatMap.java
+++ src/main/java/flowable/a_operate/flatMap.java
@@ -1,19 +1,27 @@
 package flowable.a_operate;
 
 import io.reactivex.Flowable;
-import io.reactivex.schedulers.Schedulers;
 
 
 public class flatMap {
 
-    public static void main(String[] args) {
-        Flowable.range(1, 10)
-                .observeOn(Schedulers.computation())
-                .flatMap(v -> {
-                    // new flowable
-                    return Flowable.just(v)
-                            .map(w -> w * w);
-                })
-                .blockingSubscribe(System.out::println);
+    public static Flowable<Integer> square(int v) {
+        return Flowable.just(v).map(w -> w * w);
     }
+
+    public static Flowable<Integer> twin(int v) {
+        return Flowable.just(v, v);
+    };
+
+    public static void main(String[] args) {
+
+        Flowable<Integer> f1 = Flowable.just(1,2);
+        Flowable<Integer> f2 = f1.flatMap(v -> square(v));
+        Flowable<Integer> f3 = f2.flatMap(v -> twin(v));
+
+        f1.subscribe(v1 -> System.out.println("f1 : " + v1));
+        f2.subscribe(v1 -> System.out.println("f2 : " + v1));
+        f3.subscribe(v1 -> System.out.println("f3 : " + v1));
+    }
+
 }
